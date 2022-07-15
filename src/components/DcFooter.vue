be891cbc24c855d18c304d6c11d87430434a9158
saff-it <saff_it@ymail.com>
2022-07-15 18:18:32 +0200

    Footer - bottom footer done, big logo in top footer still to be fixed

--- src/components/DcFooter.vue
+++ src/components/DcFooter.vue
@@ -66,7 +66,20 @@
     </section>
 
     <section class="container-fluid ms_bottom-footer">
-        <div class="container">
+        <div class="container ms_bottom-footer-cont">
+
+            <div class="ms_footer-button">
+                <button>Sign-up now!</button>
+            </div>
+
+            <div class="ms_social-icons">
+                <span>Follow us</span>
+                <img src="../assets/img/footer-facebook.png" alt="Fb icon">
+                <img src="../assets/img/footer-twitter.png" alt="Fb icon">
+                <img src="../assets/img/footer-youtube.png" alt="Fb icon">
+                <img src="../assets/img/footer-pinterest.png" alt="Fb icon">
+                <img src="../assets/img/footer-periscope.png" alt="Fb icon">
+            </div>
 
         </div>
     </section>
@@ -107,6 +120,8 @@ div.ms_footer-list-menu{
 
 h4{
     color: white;  
+    text-transform: uppercase;
+
 }
 
 ul{
@@ -123,6 +138,43 @@ ul{
 section.ms_bottom-footer{
     height: 100px;
     background-color: $footerColor;
+    @include flex-hor-center;
+
+}
+
+div.ms_bottom-footer-cont{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+div.ms_footer-button button{
+    padding: 10px 12px;
+    text-transform: uppercase;
+    border: 2px solid $brandColor;
+    background-color: $footerColor;
+    color: white;
+}
+
+div.ms_social-icons{
+   
+    
+    span{
+        color: $brandColor;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin-right: 12px;
+
+    }
+
+    img{
+        margin: 0 8px;
+        height: 30px;
+        
+
+    }
+
+
 }
 
 </style>
